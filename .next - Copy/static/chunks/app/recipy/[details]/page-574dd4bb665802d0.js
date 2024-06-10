8e69b5687bfe6451e606d6a8aa3ebda10d7fe845
Trambadiya-suhrad369 <152454486+Trambadiya-suhrad369@users.noreply.github.com>
2024-06-10 02:36:53 -0400

    Your commit message here

--- .next - Copy/static/chunks/app/recipy/[details]/page-574dd4bb665802d0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[609],{27554:function(e,n,s){Promise.resolve().then(s.t.bind(s,38173,23)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[173,231,971,23,744],function(){return e(e.s=27554)}),_N_E=e.O()}]);
